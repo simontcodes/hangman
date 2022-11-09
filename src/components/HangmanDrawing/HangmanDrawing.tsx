9b13226f19570081b4e20ba0dd2c8674038bf175
simon codes <simont.codes@gmail.com>
2022-11-09 09:12:55 -0500

    hangman body done

--- src/components/HangmanDrawing/HangmanDrawing.tsx
+++ src/components/HangmanDrawing/HangmanDrawing.tsx
@@ -3,10 +3,23 @@ import "./HangmanDrawing.scss";
 
 type Props = {};
 
+const HEAD = <div className="head" />;
+const BODY = <div className="body" />;
+const RIGHT_ARM = <div className="right-arm" />;
+const LEFT_ARM = <div className="left-arm" />;
+const RIGHT_LEG = <div className="right-leg" />;
+const LEFT_LEG = <div className="left-leg" />;
+
 export default function HangmanDrawing({}: Props) {
   return (
     <div className="hangman">
-      <div className="hangman__tree-top" />
+      {HEAD}
+      {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
+      <div className="hangman__noose" />
       <div className="hangman__tree-top" />
       <div className="hangman__tree" />
       <div className="hangman__base" />
